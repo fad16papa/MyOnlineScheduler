5d032d34fcd29926b0a6368dcccfe0fbcc558281
fad16papa <fad16papa@gmail.com>
2020-07-15 12:09:02 +0800

    Initial Commit

--- myonlinescheduler-api/Microservices/Activity/Domain/MyOnlineScheduler-API.Activity.Domain/Models/Photo.cs
+++
@@ -1,9 +0,0 @@
-namespace MyOnlineScheduler_API.Activity.Domain.Models
-{
-    public class Photo
-    {
-        public string Id { get; set; }
-        public string Url { get; set; }
-        public bool IsMain { get; set; }
-    }
-}
