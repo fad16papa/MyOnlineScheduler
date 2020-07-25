b7dd5342db6297c1e8442601f46ae970c57ff24f
fad16papa <fad16papa@gmail.com>
2020-07-25 17:33:10 +0800

    Commit changes

---
+++ myonlinescheduler-api/Application/Activity/Create.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Interface;
+using FluentValidation;
+using MediatR;
+using Persistence;
+
+namespace Application.Activity
+{
+    public class Create
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Title).NotEmpty();
+                RuleFor(x => x.Description).NotEmpty();
+                RuleFor(x => x.Category).NotEmpty();
+                RuleFor(x => x.Date).NotEmpty();
+                RuleFor(x => x.City).NotEmpty();
+                RuleFor(x => x.Venue).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _dataContext;
+            private readonly IUserAccessor _userAccessor;
+
+            public Handler(DataContext dataContext, IUserAccessor userAccessor)
+            {
+                _dataContext = dataContext;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = new Domain.Activity
+                {
+                    Id = request.Id,
+                    Title = request.Title,
+                    Description = request.Description,
+                    Category = request.Category,
+                    Date = request.Date,
+                    City = request.City,
+                    Venue = request.Venue
+                };
+
+                _dataContext.Activities.Add(activity);
+
+                var result = await _dataContext.SaveChangesAsync() > 0;
+
+                if (result)
+                {
+                    return Unit.Value;
+                }
+                else
+                {
+                    throw new Exception("Prolem creating an Activity");
+                }
+            }
+        }
+    }
+}
